683cc7ef1b560e9503af974e106550381ad77f38
Adelia Nainggolan <adelianainggolan25@gmail.com>
2020-10-09 09:39:04 +0700

    Initial commit

---
+++ app/src/main/java/com/adelia/nainggolan/marketplaceapp/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.adelia.nainggolan.marketplaceapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adelia.nainggolan.marketplaceapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
